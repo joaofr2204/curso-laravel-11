c08787d14f476928bef6f53415ca08a3b78b1f20
joaofrlinux <joaofr@whbbrasil.com.br>
2024-12-30 16:55:28 -0300

    view core actions

---
+++ resources/views/core/crud-actions.blade.php
@@ -0,0 +1,10 @@
+<div class="flex justify-center space-x-2">
+    <a href="{{ route('users.show', $user->id) }}" title="Visualizar"
+        class="inline-flex items-center px-2 py-2 bg-blue-500 text-white text-sm font-medium rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 transition">
+        <i class="fas fa-eye"></i>
+    </a>
+    <a href="{{ route('users.edit', $user->id)  }}" title="Editar"
+        class="inline-flex items-center px-2 py-2 bg-yellow-500 text-white text-sm font-medium rounded hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 transition">
+        <i class="fas fa-edit"></i>
+    </a>
+</div>
