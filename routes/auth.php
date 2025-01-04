ecf7591d0d5acdad9ebc31f117d75544bc520d1e
joaofr@whbbrasil.com.br <joaofr@whbbrasil.com.br>
2025-01-03 22:12:16 -0300

    tradução pt-br, index tela de login e mais

--- routes/auth.php
+++ routes/auth.php
@@ -17,10 +17,12 @@ Route::middleware('guest')->group(function () {
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])
+    ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::get('/', [AuthenticatedSessionController::class, 'create'])
+        ->name('index');
+
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
 
+require __DIR__.'/auth.php';
+
 Route::middleware('auth')->group(function () {
 
     UserController::routes();
@@ -12,22 +14,17 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-});
+    Route::get('/toggle-dark-mode', function () {
+        // Alternar entre os modos e salvar na sessão
+        $currentMode = Session::get('dark_mode', false);
+        Session::put('dark_mode', !$currentMode);
+    
+        return back(); // Redireciona para a página anterior
+    })->name('toggle-dark-mode');
 
-Route::get('/toggle-dark-mode', function () {
-    // Alternar entre os modos e salvar na sessão
-    $currentMode = Session::get('dark_mode', false);
-    Session::put('dark_mode', !$currentMode);
-
-    return back(); // Redireciona para a página anterior
-})->name('toggle-dark-mode');
-
-Route::get('/', function () {
-    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
