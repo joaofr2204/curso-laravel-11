fdc917564a1c020205c0cc991f29bc565cef13f6
João Felipe da Rosa <joaofr@whbbrasil.com.br>
2025-01-07 20:19:37 -0300

    git add .! RESIZE COLUMNS, FANTÁSTICO git add .!

--- resources/js/core/crud-index.js
+++ resources/js/core/crud-index.js
@@ -8,7 +8,7 @@ import '/resources/css/core/crud-index.css' // deixa as customizacoes de css por
 import JSZip from 'jszip'; // Para exportação para Excel
 import pdfMake from 'pdfmake/build/pdfmake'; // Para exportação para PDF
 import { route } from 'ziggy-js';
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
 
 // Tornando as dependências globais
 window.JSZip = JSZip;
@@ -16,8 +16,22 @@ window.pdfMake = pdfMake;
 
 //-- crud datatables initaialization
 $(document).ready(function () {
+
     var table = $('#crud-table').DataTable({
         dom: 'Bflrtip', // Adiciona os botões de exportação
+        pageLength: 100, // Define a quantidade de registros por página
+        lengthMenu: [100, 200, 500], // Opções para o usuário selecionar o número de registros a exibir
+        scrollY: 'calc(100vh - 303px)', // Define a altura para o grid ficar full screen
+        scrollCollapse: true, // Permite que a tabela encolha quando houver menos dados
+        select: true,
+        processing: true,
+        serverSide: true,
+
+        responsive: true,
+        autoWidth: false,
+
+        ajax: $('#crud-datatables-index-route').val(),
+        order: [[0, "asc"]], // Ordena pelo primeiro campo
         buttons: [
             {
                 extend: 'excelHtml5', // Extensão para exportar para Excel
@@ -26,20 +40,10 @@ $(document).ready(function () {
                 className: 'dt-button text-sm px-3 py-2 bg-green-500 text-white rounded-sm hover:bg-green-600 mr-2'
             }
         ],
-        pageLength: 100, // Define a quantidade de registros por página
-        lengthMenu: [100, 200, 500], // Opções para o usuário selecionar o número de registros a exibir
-        scrollY: 'calc(100vh - 303px)', // Define a altura para o grid ficar full screen
-        scrollCollapse: true, // Permite que a tabela encolha quando houver menos dados
-        select: true,
-        processing: true,
-        serverSide: true,
-        ajax: $('#crud-datatables-index-route').val(),
         columns: [
             { data: 'name', name: 'name' },
             { data: 'email', email: 'name' },
         ],
-        order: [[0, "asc"]], // Ordena pelo primeiro campo
-
         pagingType: "simple_numbers",  // Use "simple" para uma paginação mais compacta
         language: {
             "paginate": {
@@ -66,7 +70,21 @@ $(document).ready(function () {
 
             // Caso queira evitar que quebre linha, forçando o layout em uma linha
             // $('dt-container').addClass('flex flex-nowrap items-center justify-between');
+            // Prevenir a ordenação enquanto redimensiona
 
+            $('#crud-table').closest('.dt-scroll').find('.dt-scroll-headInner>table th').on('click', function (e) {
+                if (isResizing) {
+                    e.stopPropagation();// Impede a ordenação se estiver redimensionando
+                }
+            });
+
+
+            $('.dt-scroll-body').on('scroll', function () {
+                // Sincroniza o scroll horizontal do cabeçalho com o corpo
+                const scrollLeft = $(this).scrollLeft();
+                $('.dt-scroll-headInner').scrollLeft(scrollLeft);
+            });
+            
         }
 
     });
@@ -76,7 +94,76 @@ $(document).ready(function () {
         table.columns.adjust().draw();
     });
 
-    //-- CONTROLE DA SELECAO DA LINHA
+    $(window).on('resize',function(){
+        table.columns.adjust().draw();
+    });
+
+    //-- RESIZE COLUMNS CONTROL
+
+    let startX, startWidth, newWidth;
+    let isResizing = false; // Flag para rastrear se o redimensionamento está em andamento
+
+    // Função para redimensionar as colunas
+    const resizeColumns = () => {
+
+        let tab_head = $('#crud-table').closest('.dt-scroll').find('.dt-scroll-headInner>table');
+
+        tab_head.find('th').append(
+            $('<div>').addClass('dt-column-resizer')
+        ).find('.dt-column-resizer').on('mousedown', function (e) {
+
+            isResizing = true; // Inicia o redimensionamento
+            startX = e.pageX;
+            let startTableWidth = tab_head.width();
+
+            const columnIndex = $(this).parent('th').index(); // Índice da coluna
+
+            startWidth = tab_head.find('colgroup').find('col').eq(columnIndex).width();
+            
+            // Ao arrastar o mouse, redimensiona a coluna
+            $(document).on('mousemove', function (e) {
+                const diff = e.pageX - startX; // Diferença do movimento
+                newWidth = Math.round(startWidth + diff, 0);
+                
+                // console.log('pX: '+e.pageX+' sW: '+startWidth+ ' diff: '+diff+' nW: '+newWidth);
+
+                // Redimensiona o cabeçalho
+                tab_head.find('colgroup').find('col').eq(columnIndex).css('width', `${newWidth}px`);
+                tab_head.css('width', `${startTableWidth + diff}px`);
+            });
+
+            // Ao soltar o mouse, para o redimensionamento
+            $(document).on('mouseup', function (e) {
+
+                $(document).off('mousemove');
+                $(document).off('mouseup');
+
+                // Redimensiona o corpo da tabela
+                $('#crud-table tbody tr').each(function () {
+                    $(this).closest('table').find('colgroup').find('col').eq(columnIndex).css('width', newWidth + 'px');
+                });
+                $('#crud-table').css('width',`${tab_head.width()}px`);
+
+                //-- FAZ O AJUSTE FINO FINAL
+                setTimeout(function () {
+                    //table.columns.adjust();
+                    isResizing = false; // Termina o redimensionamento
+                }, 200);
+
+            });
+        });
+    };
+
+    // Iniciar a funcionalidade de redimensionamento
+    resizeColumns();
+
+
+
+
+
+
+
+    //-- SELECT LINE CONTROL
 
     window.selectedRow = null;
 
